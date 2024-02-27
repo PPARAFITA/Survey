46b568fabb16ad53f24a7ffb0a65472b41fbb6fc
Priscila Parafita <priscila.parafita56@gmail.com>
2024-02-27 15:58:56 +0100

    landing page

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,13 @@ import './App.css';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Header from './components/header'
+import Landing from './components/landing'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-
-        <Stack spacing={2} direction="row">
-          <Button variant="text">Text</Button>
-          <Button variant="contained">Contained</Button>
-          <Button variant="outlined">Outlined</Button>
-        </Stack> */}
+      <Landing/>
   
 
    
