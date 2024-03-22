a11307d9eab48e80196b87e776b3ead97802fe3a
Nahiko Arraiza <nahiko.arraiza2@volkswagen-groupservices.com>
2024-03-22 11:21:39 +0100

    Added open-api code generator

--- backend/src/main/java/com/sqa/thermometer/CorsConfig.java
+++ backend/src/main/java/com/sqa/thermometer/CorsConfig.java
@@ -1,6 +1,11 @@
-@Configuration
+package com.sqa.thermometer;
 
-public class CorsConfig implements WebMvcConfigurer{
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
 
 
 
