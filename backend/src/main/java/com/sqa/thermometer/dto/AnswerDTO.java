125ff93716dbae4ac43df2bc8e64abada8dde6da
Priscila Parafita <priscila.parafita56@gmail.com>
2024-04-15 14:49:57 +0200

    merge backend and front

--- backend/src/main/java/com/sqa/thermometer/dto/AnswerDTO.java
+++ backend/src/main/java/com/sqa/thermometer/dto/AnswerDTO.java
@@ -1,5 +1,5 @@
 package com.sqa.thermometer.dto;
- 
+
 import com.sqa.thermometer.model.Answer;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +7,7 @@ import java.util.UUID;
 import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 import java.util.UUID;
- 
+
 @Data
 @NoArgsConstructor
 public class AnswerDTO {
@@ -20,37 +20,22 @@ public class AnswerDTO {
     @JdbcTypeCode(SqlTypes.CHAR)
     private UUID questionId;
     
-<<<<<<< HEAD
-    @JdbcTypeCode(SqlTypes.CHAR)
-    private UUID teamId;    
- 
-=======
     // @JdbcTypeCode(SqlTypes.CHAR)
     // private UUID teamId;    
 
->>>>>>> f27d39f (Fix error in AnswerDTO)
     @JdbcTypeCode(SqlTypes.CHAR)
     private UUID optionId;
    
     private String valorAnswer;
- 
+
     public AnswerDTO(Answer answer){
         this.answerId = answer.getAnswerId().getId();
         this.questionId = answer.getQuestion().getQuestionId();
         this.surveyId = answer.getSurvey().getSurveyId();
-<<<<<<< HEAD
-        if(answer.getOption() != null){
-            this.setOptionId(answer.getOption().getOptionId());
-        }
- 
-        this.valorAnswer = answer.getValorAnswer();
-       
-=======
         this.valorAnswer = answer.getValorAnswer();
 
         if(answer.getOption() != null){
             this.setOptionId(answer.getOption().getOptionId());
         }
->>>>>>> f27d39f (Fix error in AnswerDTO)
     }
 }
--- frontend/src/common/components/index.ts
+++
@@ -1,8 +0,0 @@
- export * from './molecules';
- export * from './atom';
-  
- 
-
- 
-
- 
