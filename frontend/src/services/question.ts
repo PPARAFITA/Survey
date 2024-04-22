d0cb309abb2ee63dae823b9cc6fcc5f085df68cb
Patricia <moyanopg@gmail.com>
2024-04-22 17:16:27 +0200

    Update GetQuestion and answerTotal

--- frontend/src/services/question.ts
+++ frontend/src/services/question.ts
@@ -4,11 +4,15 @@ const username = 'user';
 const password = '7f57edd8-3589-48e9-beb0-f882da413aeb';
 const credentials = btoa(`${username}:${password}`);
 
-export const getQuestions = () => {
-return axios.get('/api/v1/thermometer/question', {
-    headers: {
-        'Authorization': `Basic ${credentials}`
-    }
-})
+ 
+
+export const getQuestions = (teamId: string) => {
+
+    console.log(teamId)
+    return axios.get(`/api/v1/thermometer/question/${teamId}`, {
+        headers: {
+            'Authorization': `Basic ${credentials}`
+        }
+    })
 
 }
